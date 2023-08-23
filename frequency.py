98fae15a0d741b065b1175e8c86afa56ecd46fb5
Raut <chinmay0raut@gmail.com>
2023-08-23 09:58:45 -0400

    improved documentation of code.py

---
+++ frequency.py
@@ -0,0 +1,30 @@
+# this script contains two functions to count the frequency of characters in a string
+
+from collections import Counter
+
+def count_frequency(string: str) -> dict:
+    """
+    This function counts the frequency of each character in a string and returns a dictionary.
+
+    Args:
+        string (str): The string to count the frequency of characters.
+
+    Returns:
+        dict: A dictionary containing the frequency of each character in the string.
+    """
+    return Counter(string)
+
+
+def print_frequency(frequency_dict: dict) -> None:
+    """
+    This function takes in a dictionary containing the frequency of each character and prints the frequency of each key.
+
+    Args:
+        frequency_dict (dict): A dictionary containing the frequency of each character.
+    """
+    print('Frequency of characters:')
+    total = float(sum([cnt for cnt in frequency_dict.values()]))
+    for char in frequency_dict.keys():
+        print(char + ':' + str(frequency_dict[char]/total))
+
+print_frequency(count_frequency('ATCTGACGCGCGCCGC'))
